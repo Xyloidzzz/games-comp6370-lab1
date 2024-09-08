88364270940cba0d7b8707017c8df01ad326f41f
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2024-09-08 15:44:28 -0500

    updated global const names
    for better clarity

--- twixt_board.py
+++ twixt_board.py
@@ -11,10 +11,10 @@
 from graphics import *
 
 # global variables  
-boardSize = 7
-lineWidth = 4
-centers = []
-clickAreas = []
+BOARD_SIZE = 7
+LINE_WIDTH = 4
+CENTERS = []
+CLICKABLE_AREAS = []
 
 def draw_board(window):
     # draw the board and save the clickable areas
@@ -22,26 +22,26 @@ def draw_board(window):
         for j in range(50, 800, 100):
             rect = Rectangle(Point(i - 50, j - 50), Point(i + 50, j + 50))
             rect.draw(window)
-            clickAreas.append(rect)
+            CLICKABLE_AREAS.append(rect)
     
     # draw the border lines
     line = Line(Point(120, 100), Point(580, 100)) # bottom
-    line.setWidth(lineWidth)
+    line.setWidth(LINE_WIDTH)
     line.draw(window)
     line = Line(Point(100, 120), Point(100, 580)) # left
-    line.setWidth(lineWidth)
+    line.setWidth(LINE_WIDTH)
     line.draw(window)
     line = Line(Point(120, 600), Point(580, 600)) # top
-    line.setWidth(lineWidth)
+    line.setWidth(LINE_WIDTH)
     line.draw(window)
     line = Line(Point(600, 120), Point(600, 580)) # right
-    line.setWidth(lineWidth)
+    line.setWidth(LINE_WIDTH)
     line.draw(window)
     
     # draw the peg holes
     for i in range(50, 800, 100):
         for j in range(50, 800, 100):
-            centers.append(Point(i, j)) # save the center points of the peg holes
+            CENTERS.append(Point(i, j)) # save the center points of the peg holes
             circle = Circle(Point(i, j), 40)
             circle.draw(window)
 
@@ -57,15 +57,15 @@ def play_game(window):
         peg2 = Point(0, 0)
         
         # draw a peg in the center corresponding to the clicked area
-        for i in range(len(clickAreas)): 
-            if clickAreas[i].getP1().getX() < click1.getX() < clickAreas[i].getP2().getX() and clickAreas[i].getP1().getY() < click1.getY() < clickAreas[i].getP2().getY():
-                peg1 = Circle(centers[i], 10)
-                peg = Circle(centers[i], 10)
+        for i in range(len(CLICKABLE_AREAS)): 
+            if CLICKABLE_AREAS[i].getP1().getX() < click1.getX() < CLICKABLE_AREAS[i].getP2().getX() and CLICKABLE_AREAS[i].getP1().getY() < click1.getY() < CLICKABLE_AREAS[i].getP2().getY():
+                peg1 = Circle(CENTERS[i], 10)
+                peg = Circle(CENTERS[i], 10)
                 peg.setFill("black")
                 peg.draw(window)
-            if clickAreas[i].getP1().getX() < click2.getX() < clickAreas[i].getP2().getX() and clickAreas[i].getP1().getY() < click2.getY() < clickAreas[i].getP2().getY():
-                peg2 = Circle(centers[i], 10)
-                peg = Circle(centers[i], 10)
+            if CLICKABLE_AREAS[i].getP1().getX() < click2.getX() < CLICKABLE_AREAS[i].getP2().getX() and CLICKABLE_AREAS[i].getP1().getY() < click2.getY() < CLICKABLE_AREAS[i].getP2().getY():
+                peg2 = Circle(CENTERS[i], 10)
+                peg = Circle(CENTERS[i], 10)
                 peg.setFill("black")
                 peg.draw(window)
         # draw a line between the two clicks
@@ -76,7 +76,7 @@ def play_game(window):
         
 def main():
     window = GraphWin("Twixt", 1000, 1000)
-    window.setCoords(0, 0, boardSize * 100, boardSize * 100)
+    window.setCoords(0, 0, BOARD_SIZE * 100, BOARD_SIZE * 100)
     window.setBackground("white")
     draw_board(window)
     play_game(window)
